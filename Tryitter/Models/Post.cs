70afd3bf4211304988835c9b543dc8390bcbf50b
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-10-21 00:32:54 -0300

    :sparkles: feat: Define propriedades opcionais da tabela Users

--- Tryitter/Models/Post.cs
+++ Tryitter/Models/Post.cs
@@ -3,8 +3,8 @@ namespace Tryitter.Models
     public class Post
     {
         public int PostId { get; set; }
-        public string Content { get; set; }
-        public string ImageUrl { get; set; }
+        public string? Content { get; set; }
+        public string? ImageUrl { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
     }
--- Tryitter/Models/User.cs
+++ Tryitter/Models/User.cs
@@ -5,8 +5,8 @@ namespace Tryitter.Models
         public int UserId { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string Password { get; set; }
-        public ICollection<Post> Posts { get; }
+        public ICollection<Post>? Posts { get; }
     }
 }
