dd407d5a4da6db2121bcd2aa1b65fb96094bb4ab
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-11-01 19:22:51 -0300

    :sparkles: feat: Adicionando post repository e useCase no program

--- Tryitter/Program.cs
+++ Tryitter/Program.cs
@@ -1,17 +1,24 @@
 using Tryitter.Models;
 using Tryitter.Repositories;
 using Tryitter.UseCases;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 DotNetEnv.Env.Load();
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => 
+{ 
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.WriteIndented = true;
+});
 builder.Services.AddDbContext<TryitterContext>();
 builder.Services.AddScoped<TryitterContext>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserUseCase, UserUseCase>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IPostUseCase, PostUseCase>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
