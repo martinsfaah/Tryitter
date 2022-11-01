00819905e31b0376c9a2d6fa418a409d4f5903f4
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-10-31 21:28:24 -0300

    :recycle: refactor: Alterando a model de user

--- Tryitter/Models/User.cs
+++ Tryitter/Models/User.cs
@@ -5,7 +5,7 @@ namespace Tryitter.Models
         public int UserId { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string Password { get; set; }
         public ICollection<Post>? Posts { get; }
     }
