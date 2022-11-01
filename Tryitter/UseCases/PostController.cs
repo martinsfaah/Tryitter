9855a8d96e92e453a191b85711df81c667fc10f4
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-11-01 19:13:48 -0300

    :sparkles: feat: Adicionando create post controller

---
+++ Tryitter/UseCases/PostController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+
+using Tryitter.UseCases;
+using Tryitter.Models;
+using Tryitter.RequestHandlers;
+namespace Tryitter.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class PostController : ControllerBase
+{
+  private readonly IPostUseCase _postUseCase;
+  public PostController(IPostUseCase postUseCase)
+  {
+    _postUseCase = postUseCase;
+  }
+
+  [HttpPost]
+  [AllowAnonymous]
+  public ActionResult<Post> Create([FromBody] Post post)
+  {
+    try
+    {
+      var created = _postUseCase.Create(post);
+
+      if (created is null)
+      {
+        return NotFound("User not found");
+      }
+
+      return CreatedAtAction("GetById", new { id = created.PostId }, created);
+    }
+    catch (Exception exception)
+    {
+      return BadRequest(exception.Message);
+    }
+  }
+
+
+}
