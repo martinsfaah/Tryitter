58c35a83d5cb253ac34919e22af1c1c525720db5
fabiana <fabianamfranco@outlook.com>
2022-11-02 21:49:50 -0300

    :wrench: chore: Configuração inicial

---
+++ tryitter.front/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
+// import { Provider } from './context/Provider';
+
+import Login from './pages/Login';
+// import Register from './pages/Register';
+// import Feed from './pages/Feed';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route exact path="/" element={<Navigate replace to="/login" />} />
+        {/* <Route path="/feed" element={<Feed />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
+      </Routes>
+    </Router>
+  );
+}
