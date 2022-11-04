e24f3afad7c000d2acbcf1fd490b99c27b62b714
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-11-03 21:57:50 -0300

    :construction: Adiciona a propriedade role ao User

--- Tryitter/Models/User.cs
+++ Tryitter/Models/User.cs
@@ -7,6 +7,7 @@ namespace Tryitter.Models
         public string Email { get; set; }
         public string? Name { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; }
         public ICollection<Post>? Posts { get; }
     }
 }
